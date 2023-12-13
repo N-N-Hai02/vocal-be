82ab0625226099ec6e5e52d052e1e53040efb35c
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-13 14:39:39 +0700

    Add fix set cookies - v1

--- src/controllers/user/apiController.js
+++ src/controllers/user/apiController.js
@@ -47,7 +47,7 @@ const handleApiLogin = async (req, res) => {
     try {
         let data = await loginRegisterService.handleUserLogin(req.body)
         if (data && data.DT && data.DT.access_token) {
-            res.cookie("jwt", data.DT.access_token, { secure: true, httpOnly: true, maxAge: 60 * 60 * 1000 })
+            res.cookie("jwt", data.DT.access_token, { secure: true, httpOnly: true, sameSite: 'none', maxAge: 60 * 60 * 1000 })
         }
         return res.status(200).json({
             EM: data.EM, // error message
