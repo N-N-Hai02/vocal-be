0546e9998cbbc5a2d148559933ef711730e44155
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-21 14:26:29 +0700

    Updated cors..!

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,7 +4,7 @@ const configCors = (app) => {
     // Add headers before the routes are defined
     app.use(function (req, res, next) {
         // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+        res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL ? process.env.REACT_URL : 'http://localhost:3000');
 
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
