fbab1beed9b23154b8aa47acb6294bbbe9a5c82d
NguyenNgocHaik2 <haiboonguyenk2@gmail.com>
2024-02-16 15:07:22 +0700

    fix error connect supabase production - v1

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, 
+    "postgres", // process.env.DB_DATABASE_NAME,
+    "postgres", // process.env.DB_USERNAME,
+    "hai@123VocalAppmnbvcxz", // process.env.DB_PASSWORD, 
     {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        host: "db.kbngeposzxnvbbnceppd.supabase.co", // process.env.DB_HOST,
+        port: 5432, // process.env.DB_PORT,
+        dialect: "postgres", // process.env.DB_DIALECT,
         dialectOptions: 
         process.env.DB_SSL === 'true' 
         ? 
