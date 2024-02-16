4ead8babedd570c0f3c93cb4bdbeab6d8c9336b0
NguyenNgocHaik2 <haiboonguyenk2@gmail.com>
2024-02-16 21:55:22 +0700

    Test fix bug connect supabase - deploy

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -23,7 +23,13 @@ const sequelize = new Sequelize(
         query: {
             "raw": true
         },
-        timezone: "+07:00"
+        timezone: "+07:00",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
     }
 );
 
