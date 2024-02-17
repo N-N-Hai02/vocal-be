f505e3605e988b93435d216d5ba843ca06fd322c
NguyenNgocHaik2 <haiboonguyenk2@gmail.com>
2024-02-17 22:01:19 +0700

    Fix bug - deploy connected supabase

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,31 +1,72 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, 
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        dialectOptions: 
-        process.env.DB_SSL === 'true' 
-        ? 
+const {
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE_NAME,
+    DB_DIALECT,
+    NODE_ENV
+} = process.env;
+
+// const sequelize = new Sequelize(
+//     process.env.DB_DATABASE_NAME,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD, 
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: process.env.DB_DIALECT,
+//         dialectOptions: 
+//         process.env.DB_SSL === 'true' 
+//         ? 
+//         {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false
+//             }
+//         }
+//         :
+//         {},
+//         query: {
+//             "raw": true
+//         },
+//         timezone: "+07:00"
+//     }
+// );
+
+let sequelize;
+sequelize = NODE_ENV === "production"
+    ? new Sequelize(
         {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
+            dialect: DB_DIALECT,
+            host: DB_HOST,
+            port: DB_PORT,
+            database: DB_DATABASE_NAME,
+            username: DB_USERNAME,
+            password: DB_PASSWORD,
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            },
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                },
+                keepAlive: true,
+            },
+            ssl: true,
         }
-        :
-        {},
-        query: {
-            "raw": true
-        },
-        timezone: "+07:00"
-    }
-);
+    )
+    : new Sequelize(
+        `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE_NAME}`,
+        { logging: false, native: false }
+    );
 
 const connection = async () => {
     try {
